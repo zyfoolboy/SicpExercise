d9eb804c334c0523c9d44ac4d2c0d4fb2f7a0222
zyfoolboy <zyfoolboy@gmail.com>
2016-09-20 15:09:14 +0800

    1-8

--- 1-8.scm
+++ 1-8.scm
@@ -1,23 +1,22 @@
-(define (new-if	 predicate then-clause else-clause)
-  ((cond (predicate then-clause)
-        (else else-clause))))
+(define (cbrt x)
+	(define (good-enough? guess new-guess)
+		(< (/ (abs (- guess new-guess)) guess) 0.0001))
+
+	(define (improve guess x)
+	  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+
+	(define (cbrt-iter guess x)
+		(if (good-enough? guess (improve guess x))
+			(improve guess x)
+			(cbrt-iter (improve guess x) x)))
+
+	(cbrt-iter 1.0 x))
 
 (define (good-enough? guess x)
 	(< (abs (- (square guess) x)) 0.001))
 
-(define (average x y)
-	(/ (+ x y) 2))
-
 (define (improve guess x)
 	(average guess (/ x guess)))
 
-(define (sqrt-iter guess x)
-	(new-if (good-enough? guess x)
-		guess
-		(sqrt-iter (improve guess x)
-					x)))
 
-(define (sqrt x)
-	(sqrt-iter 1.0 x))
-
-(sqrt 2)
+(cbrt 27)
