8ce1456ec815f7a92e8b1ea7f386e51642c6b44c
zyfoolboy <deyi@deyideMac-mini.local>
2016-09-20 15:06:43 +0800

    1-7

--- 1-8.scm
+++ 1-8.scm
@@ -1,3 +1,7 @@
+(define (new-if	 predicate then-clause else-clause)
+  ((cond (predicate then-clause)
+        (else else-clause))))
+
 (define (good-enough? guess x)
 	(< (abs (- (square guess) x)) 0.001))
 
@@ -8,7 +12,7 @@
 	(average guess (/ x guess)))
 
 (define (sqrt-iter guess x)
-	(if (good-enough? guess x)
+	(new-if (good-enough? guess x)
 		guess
 		(sqrt-iter (improve guess x)
 					x)))
@@ -16,9 +20,4 @@
 (define (sqrt x)
 	(sqrt-iter 1.0 x))
 
-(sqrt 9)
-(sqrt 10)
-
-(good-enough? 1 (/ 900000000000000000000000000000000000000000000000000000000000000000000000000000000001 2))
-
-(sqrt 900000000000000000000000000000000000000000000000000000000000000000000000000000000000)
+(sqrt 2)
