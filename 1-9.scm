b676f41a4824fc8f45afccccfa8a49f7b6146cca
zyfoolboy <zyfoolboy@gmail.com>
2016-11-11 10:32:01 +0800

    add

--- 1-9.scm
+++ 1-9.scm
@@ -1,22 +1,16 @@
-(define (cbrt x)
-	(define (good-enough? guess new-guess)
-		(< (/ (abs (- guess new-guess)) guess) 0.0001))
+(define (+ a b)
+	(if (= a 0)
+		b
+		(inc (+ （dec a) b)))
 
-	(define (improve guess x)
-	  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
+(define (+ a b)
+	(if (= a 0)
+		b
+		(+ (dec a) (inc b))))
 
-	(define (cbrt-iter guess x)
-		(if (good-enough? guess (improve guess x))
-			(improve guess x)
-			(cbrt-iter (improve guess x) x)))
+(define (inc one)
+	(+ one 1))
 
-	(cbrt-iter 1.0 x))
+(define (dec one)
+  (- one 1))
 
-(define (good-enough? guess x)
-	(< (abs (- (square guess) x)) 0.001))
-
-(define (improve guess x)
-	(average guess (/ x guess)))
-
-
-(cbrt 27)
